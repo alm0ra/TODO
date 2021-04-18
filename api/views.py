b1a1dab62fb1c17c7b76c06bb06503b7117d6534
ali <ali.mrd318@gmail.com>
2021-04-19 01:10:13 +0430

    Api Overview add as First view api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+
+@api_view(['GET'])
+def apiOverview(request):
+
+    api_urls = {
+        'Task List':'/task-list/',
+        'Done List':'/done-list/',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+        'Move To Done(Make a Task Done)':'/move-done/<str:pk>/',
+        'Move To Tasks(Make a Task undone)':'/move-task/<str:pk>/',
+
+    }
+    return Response(api_urls)
