1962cd4db3c59ed697ecd194c6ebeb20e768a911
ali <ali.mrd318@gmail.com>
2021-04-19 01:51:52 +0430

    Fix Datetime item

--- api/models.py
+++ api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 class Tasks(models.Model):
-    title = models.CharField(max_length=100)
-    models.DateTimeField(auto_now_add=True, verbose_name="created date")
-    status = models.BooleanField(default=False, blank=True, null=True)
+    title = models.CharField(max_length=100, verbose_name="Task's Title")
+    created_date = models.DateTimeField(default=timezone.now, verbose_name="created date", blank=True)
+    status = models.BooleanField(default=False, blank=True, null=True, verbose_name="Task's Status")
 
     class Meta :
         verbose_name = "Tasks"
