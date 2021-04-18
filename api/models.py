6ef910c7cd3b24213c2c41ccef61e0b7ccc1cd8b
ali <ali.mrd318@gmail.com>
2021-04-18 23:53:42 +0430

    Make Task model on api app

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tasks(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="created by :")
+    title = models.CharField(max_length=100)
+    models.DateTimeField(auto_now_add=True, verbose_name="created date")
+    status = models.BooleanField(default=False, blank=True, null=True)
+
+    class Meta :
+        verbose_name = "Tasks"
+        verbose_name_plural= "Tasks"
+    def __str__(self):
+        return self.title
+    def __unicode__(self):
+        return self.title
